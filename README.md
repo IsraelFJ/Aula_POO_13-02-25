b8f9ab0d0af2ac486a5b4f403fe52a9a325f0eba
Israel Falcão <israel.jesus@ba.estudante.senai.br>
2025-02-13 23:05:12 +0000

    New commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Aula_POO_13-02-25
-### Continuação da aula de orientção a objetos Professor Carols
-#### Assusntos abordados anteriormente, no repositório encapsulamento *JAVA* onde foram criados geters e seters para criar um primeiro objeto simples.
+## Continuação da aula de orientção a objetos Professor Carlos.
+#### Assuntos abordados anteriormente, no repositório encapsulamento *JAVA* onde foram criados getters e setters para criar um primeiro objeto simples.
+##### Abordamos em 13/02/2025 conceitos de encapsulamento.
--- poo/Main.java
+++ poo/Main.java
@@ -1,8 +1,43 @@
+import java.util.Scanner;
+
+import models.Pessoa;
+
+// Autor Israel Falcão Jesus 
+
 public class Main {
 
     public static void main(String[] args) {
         
-            System.out.println("hadouken");
+            Scanner scanner = new Scanner(System.in);
+            
+            int idade;
+            String nome;
+            String email;
+            
+            // estanciando o objeto e passando paramentros vazios afim de utilizar variaveis globais para pasage de informação
+            Pessoa pessoa = new Pessoa("", "", 0);
+            
+            // utilizando variaveis locais para solicitar aos usuarios o valor para variavel
+            System.out.println("Informe seu nome: ");
+            nome = scanner.nextLine();
+            System.out.println("Informe seu E-mail: ");
+            email = scanner.nextLine();
+            System.out.println("Informe sua idade: ");
+            idade = scanner.nextInt();
+            
+            //utilizando variais locais para passar o paramentro para a estrutura do objeto
+            pessoa.setIdade(idade);
+            pessoa.setEmail(email);
+            pessoa.setNome(nome);
+            
+            // get entrega o parametro que recebel do set atraves da variavel global 
+            System.out.println("Cadastrado pessoa: "+ pessoa.getNome());
+            System.out.println("-----------------------------------");
+            System.out.println("seu email de cadastro é E-mail: "+ pessoa.getEmail());
+            System.out.println("-----------------------------------");
+            System.out.println("A sua idade é: "+ pessoa.getIdade());
+            System.out.println("-----------------------------------");
+            
 
 
     }
