9a8f2817f203157d050e338d58ff4a47cddcc828
Israel Falcão <israel.jesus@ba.estudante.senai.br>
2025-02-28 00:28:03 +0000

    Atualizando branch feature/interface

--- interface_atividade/models_atividade/Divisao.java
+++ interface_atividade/models_atividade/Divisao.java
@@ -4,8 +4,8 @@ public class Divisao implements OperacaoMatematica {
 
     @Override
     public double Calcular(double a, double b) {
-        a = 2;
-        b = 12;
+        a = 60;
+        b = 2;
         return a / b;
     }
 
