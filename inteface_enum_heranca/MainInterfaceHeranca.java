79f899a74646d092684c059ecdc92403e3277230
Israel Falcão <israel.jesus@ba.estudante.senai.br>
2025-03-09 22:47:15 +0000

    commit de ajuste de interface

--- inteface_enum_heranca/MainInterfaceHeranca.java
+++ inteface_enum_heranca/MainInterfaceHeranca.java
@@ -10,12 +10,14 @@ public class MainInterfaceHeranca {
         
         MotoboyHeranca motoboyHeranca = new MotoboyHeranca("João", "01/01/1990", 1500.00, 
         SexoEnum.MASCULINO, SetorEnum.OPERACIONAL, "123456789");
+        System.out.println("====================================");
         System.out.println(motoboyHeranca.toString());
         System.out.println("====================================");
         
         
         DiretorHeranca diretorHeranca = new DiretorHeranca("Maria", "01/01/1980", 5000.00,
         SexoEnum.FEMININO, SetorEnum.TI, 0.2);
+        System.out.println("====================================");
         System.out.println(diretorHeranca.toString());
         System.out.println("Salario final: "+diretorHeranca.getFInalSalario());
         System.out.println("====================================");
